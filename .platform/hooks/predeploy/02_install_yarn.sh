5e672ab0db011cb1e882f04ae645d8e2b66c10a8
Sumit Mishra <cr7sumitmishra@gmail.com>
2023-10-14 11:22:27 +0530

    Update 02_install_yarn.sh

--- .platform/hooks/predeploy/02_install_yarn.sh
+++ .platform/hooks/predeploy/02_install_yarn.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
-echo 'Installing a node for the yarn install...'
-curl --silent --location https://rpm.nodesource.com/setup_14.x | sudo bash -
-yum -y install nodejs
 
-echo 'Installing yarn...'
+echo 'Installing NVM (Node Version Manager)...'
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
+
+# Activate NVM
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
+# Install Node.js using NVM
+nvm install 14
+nvm use 14
+
+echo 'Installing Yarn...'
 wget https://dl.yarnpkg.com/rpm/yarn.repo -O /etc/yum.repos.d/yarn.repo
 yum -y install yarn
